54688b02842a7fadec6e31752b2bb875aff86ac2
Dan Riti <dmriti@gmail.com>
2013-01-26 00:17:53 -0500

    Add test case for adding object definition instances.

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from urllib2 import urlopen
 import simplejson
 import unittest
 
-from wrapdb import objectdef, project, utils
+from wrapdb import instance, objectdef, project, utils
+
+API_KEY = '123Key'
 
 # Test Cases!
 class WrapDBTest(unittest.TestCase):
@@ -16,14 +18,42 @@ class WrapDBTest(unittest.TestCase):
         self.assertEqual(response.get('status'), 'success')
 
     def test_create_project(self):
-        response = project.create("Krapp")
+        response = project.create(API_KEY, "Testing123")
         self.assertEqual(response.get('status'), 'success')
 
     def test_create_objectdef(self):
-        objectDef = {'data' : [{'name' : 'name', 'type' : 'string'},
-                               {'name' : 'address', 'type' : 'string'}]}
+        objectDefData = {'data' : [{'name' : 'name', 'type' : 'string'},
+                                   {'name' : 'address', 'type' : 'string'}]}
 
-        response = objectdef.create('123Key', 'business', objectDef)
+        response = objectdef.create(API_KEY, 'business', objectDefData)
+        self.assertEqual(response.get('status'), 'success')
+
+    def test_krapp(self):
+        # Create project.
+        response = project.create(API_KEY, "Krapp")
+        self.assertEqual(response.get('status'), 'success')
+
+        # Grab the project id.
+        projectId = response.get('id')
+
+        # Setup business object definition name and data.
+        objectDefName = 'business'
+        objectDefData = {'data' : [{'name' : 'name', 'type' : 'string'},
+                                   {'name' : 'address', 'type' : 'string'}]}
+
+        # Create business object.
+        response = objectdef.create(API_KEY, objectDefName, objectDefData)
+        self.assertEqual(response.get('status'), 'success')
+
+        # Setup business object instance.
+        instanceData = {'data' : {"name" : "McDonalds",
+                                  "address" : "123 Happy Meal St"}}
+
+        # Insert a business object.
+        response = instance.insert(API_KEY,
+                                   projectId,
+                                   objectDefName,
+                                   instanceData)
         self.assertEqual(response.get('status'), 'success')
 
 # Get going test runner!
